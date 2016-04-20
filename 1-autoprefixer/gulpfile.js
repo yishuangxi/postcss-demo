5b2b18c72606b2a230e2170a0fac7ca2fd7e008d
yishuangxi <yishuangxi@qiushibaike.com>
2016-04-20 19:09:32 +0800

    post-css: auto-prefixer

---
+++ 1-autoprefixer/gulpfile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by qiubai on 2016/4/19.
+ */
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('style', function(){
+    var processers = [autoprefixer];
+    return gulp.src('./src/style.css')
+        .pipe(postcss(processers))
+        .pipe(gulp.dest('./dest'));
+});
+
+// gulp.task('css', function () {
+//     var processors = [
+//     ];
+//     return gulp.src('./src/*.css')
+//         .pipe(postcss(processors))
+//         .pipe(gulp.dest('./dest'));
+// });
