0a6456e54ee87272db6fa40e90b27858d03735bb
yishuangxi <2546618112@qq.com>
2016-06-29 15:52:55 +0800

    autoprefixer

--- 1-autoprefixer/gulpfile.js
+++ 1-autoprefixer/gulpfile.js
@@ -4,20 +4,10 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var cssgrace = require('cssgrace');
-var media_minmax = require('postcss-media-minmax');
 
-gulp.task('style', function(){
-    var processers = [autoprefixer, cssgrace, media_minmax];
-    return gulp.src('./src/style.css')
+gulp.task('autoprefixer', function(){
+    var processers = [autoprefixer];
+    return gulp.src('./src/*.css')
         .pipe(postcss(processers))
         .pipe(gulp.dest('./dest'));
 });
-
-// gulp.task('css', function () {
-//     var processors = [
-//     ];
-//     return gulp.src('./src/*.css')
-//         .pipe(postcss(processors))
-//         .pipe(gulp.dest('./dest'));
-// });
