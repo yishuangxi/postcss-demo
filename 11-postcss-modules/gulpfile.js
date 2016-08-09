91300b6a1885bead1d9d8962e2a9bf6cb5816488
yishuangxi <2546618112@qq.com>
2016-08-09 11:54:38 +0800

    html list

--- 11-postcss-modules/gulpfile.js
+++ 11-postcss-modules/gulpfile.js
@@ -15,27 +15,41 @@ var posthtml = require('gulp-posthtml');
 var posthtmlCssModules = require('posthtml-css-modules')
 var dist = __dirname + '/dest'
 var concat =require('gulp-concat')
+var gulp_clean = require('gulp-clean')
 
 gulp.task('modules', function () {
     var processors = [modules({
         generateScopedName: '[name]__[local]__[hash:base64:5]',
         getJSON: function (cssFileName, json) {
             var cssName = path.basename(cssFileName, '.css');
-            var jsonFileName = path.resolve('./dest' + cssName + '.json');
+
+            var jsonFileName = path.resolve('./dest/json', cssName + '.json');
             fs.writeFileSync(jsonFileName, JSON.stringify(json));
         }
     })]
     return gulp.src('src/css/*.css')
         .pipe(postcss(processors))
-        .pipe(concat('public.css'))
+        // .pipe(concat('public.css'))
         .pipe(gulp.dest('dest/css'))
+})
+
+gulp.task('clean', function(){
+    gulp.src(['dest/css', 'dest/js', 'dest/html', 'dest/json/*.json'])
+        .pipe(gulp_clean())
 })
 
 gulp.task('html', ['modules'], function() {
     var posthtml = require('gulp-posthtml');
-    return gulp.src('src/**/*.html')
-        .pipe(posthtml([ require('posthtml-css-modules')('./deststyle.json') ]))
-        .pipe(gulp.dest('dest/'));
+
+    var htmlList = fs.readdirSync('src/html')
+
+    for (var i = 0; i < htmlList.length; i++) {
+        var filename = htmlList[i]
+        console.log(filename)
+        gulp.src('src/html/' + filename)
+            .pipe(posthtml([require('posthtml-css-modules')('./dest/json/' + filename.split('.')[0] + '.json')]))
+            .pipe(gulp.dest('dest/html'));
+    }
 });
 
 // gulp.task('posthtml', ['modules'], function() {
